118cf8efb04aa3056cf01ae1daff57293d2536a7
Michael Mathias <michaelscodingjourney@gmail.com>
2023-04-24 23:55:45 -0700

    added toy info to the cards

--- src/index.js
+++ src/index.js
@@ -13,3 +13,35 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+const andysToyCollection = document.querySelector('#toy-collection');
+
+fetch('http://localhost:3000/toys')
+.then(resp => resp.json())
+.then(andysToys => {
+  andysToys.forEach(toy => {
+    const cardDiv = document.createElement('div');
+    cardDiv.setAttribute('class', 'card');
+
+    const toyImg = document.createElement('img');
+    toyImg.src = toy.image;
+    toyImg.setAttribute('class', 'toy-avatar');
+    cardDiv.appendChild(toyImg);
+
+    const toyH2 = document.createElement('h2');
+    toyH2.textContent = toy.name;
+    cardDiv.appendChild(toyH2);
+
+    const toyP = document.createElement('p');
+    toyP.textContent = `${toy.likes} likes`;
+    cardDiv.appendChild(toyP);
+
+    const toyBtn = document.createElement('button');
+    toyBtn.setAttribute('class', 'like-button');
+    toyBtn.setAttribute('id', toy.id);
+    toyBtn.textContent = 'like';
+    cardDiv.appendChild(toyBtn);
+
+    andysToyCollection.appendChild(cardDiv);
+  });
+})
